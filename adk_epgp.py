2119f3f284d313341927b7aa1782ae0d427c7d17
Jackson McCrea <frodo2777@gmail.com>
2016-04-27 01:49:52 -0700

    Testing out Gitlab's CI

--- adk_epgp.py
+++ adk_epgp.py
@@ -1,9 +1,15 @@
 from epgp import app, shutdown
+from sys import argv
 
 if __name__ == '__main__':
-    app.debug = False
+    port = 6000
+    if '--debug' in argv:
+        app.debug = True
+        port = 5000
+    if '--ci' in argv:
+        exit(0)
     try:
-        app.run(debug=True)
+        app.run(port=port)
     except KeyboardInterrupt:
         print('Cleaning up...')
     shutdown()
